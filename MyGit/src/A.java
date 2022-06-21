ca1083c466c943d3f9a22aaa00a119aff0f6fdfa
dlthw <dlthw@DESKTOP-QM075GJ>
2022-06-21 16:31:13 +0900

    A에서 main 메서드 추가

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -3,5 +3,7 @@ public class A {
 	public static void main(String args[]) {
 		System.out.println("Hello, this is A.java!");
 		System.out.println("Let's start HW!!!");
+		
+		System.out.println("After merge");
 	}
 }
