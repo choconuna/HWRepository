7446330a743dd02747d6f021170f8b01bcc364ce
dlthw <dlthw@DESKTOP-QM075GJ>
2022-06-21 16:28:55 +0900

    C 생성자 추가, A에서 C 생성자 테스트

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -3,5 +3,8 @@ public class A {
 	public static void main(String args[]) {
 		System.out.println("Hello, this is A.java!");
 		System.out.println("Let's start HW!!!");
+		
+		C c = new C();
+		c.myC();
 	}
 }
--- MyGit/src/C.java
+++ MyGit/src/C.java
@@ -1,4 +1,6 @@
 
 public class C {
-
+	public void myC() {
+		System.out.println("This is C.java!");
+	}
 }
